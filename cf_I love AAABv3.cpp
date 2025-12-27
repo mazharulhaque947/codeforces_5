bb4c328c3161e9da6421403198cb5f8030aef3d0
mazharulhaque947 <mazharulhaque94721@gmail.com>
2025-12-27 18:41:07 +0600

    27 dec 2025 v3

---
+++ cf_I love AAABv3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k,n,m,t;
+    string a;
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>a;
+        n=a.length();
+        k=1;
+        m=0;
+        if(n<=1){k=0;}
+        if(a[n-1]=='A'){k=0;}
+        for(i=0;i<n;i++){ if(a[i]=='A'){m++;} else{m--; } if(m<0){k=0;}}
+
+        if(k==1){ cout<<"YES\n"; }
+        else{ cout<<"NO\n";  }
+
+    }
+
+    return 0;
+}
+
+
