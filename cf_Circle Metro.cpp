7e40ba328a3db4d14f7567a50133341f33ebe490
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-01-04 17:21:59 +0600

    4 jan 2026

---
+++ cf_Circle Metro.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,m,a,x,b,y,i,j,k,l,t;
+
+    cin>>n>>a>>x>>b>>y;
+    t=0;
+    k=0;
+    while(1){
+
+        if(a==b){t=1; break;}
+
+        if(a==x){break;}
+        if(b==y){break;}
+
+        a++; if(a==n+1){a=1;}
+        b--; if(b==-1){b=n;}
+
+        k++;
+        if(k>1000){break;}
+    }
+    (t==1)?cout<<"YES\n":cout<<"NO\n";
+    return 0;
+}
