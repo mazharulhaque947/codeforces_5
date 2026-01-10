f6a35183578f321b9670c21705d29e5b87b46c87
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-01-11 05:48:05 +0600

    11 jan 2026

---
+++ cf_Two Bases.cpp
@@ -0,0 +1,29 @@
+
+//A. Two Bases
+
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int pp(int a,int p)
+{
+int b=1;
+while(a--){  b=b*p; }
+return b;
+}
+int32_t main()
+{
+int n,b,c,x,y,t;
+
+x=y=0;
+cin>>n>>b;
+while(n--){  cin>>c;x=x+c*pp(n,b)   ; }
+cin>>n>>b;
+while(n--){  cin>>c;y=y+c*pp(n,b)   ; }
+//cout<<x<<" "<<y<<"\n";
+if(x==y){cout<<"=\n";   }
+else if(x>y){ cout<<">\n";   }
+else{ cout<<">\n";  }
+
+return 0;
+}
