261fe7a6d88ce57552dd016dc6157c41cf14f199
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-01-18 08:20:13 +0600

    18 jan 2026 v2

--- cf_Boboniu Likes to Color Balls.cpp
+++ cf_Boboniu Likes to Color Balls.cpp
@@ -11,7 +11,7 @@ int main()
         cin>>r[0]>>r[1]>>r[2]>>r[3];
 
         o=p=z=0;
-        for(i=0;i<4;i++){ if(r[i]%2==1){o++;}else{p++;} if(r[i]==0){z++;}}
+        for(i=0;i<4;i++){ if(r[i]%2==1){o++;}else{p++;} if(r[i]==0&&i!=3){z++;}}
 
         if(p==4){cout<<"YES\n";}
         else if(z==1&&o==3){ cout<<"NO\n"; }
