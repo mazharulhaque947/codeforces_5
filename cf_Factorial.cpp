441498e53379e4655f787230743fc61c4db3fdab
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-01-28 18:44:28 +0600

    28 jan 2026 recursion test sheet

---
+++ cf_Factorial.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long fac(long long n)
+{
+
+    if(n<=1){return 1;}
+    else{return n*fac(n-1);}
+
+}
+
+int main()
+{
+    long long n;
+    cin>>n;
+    cout<<fac(n);
+
+    return 0;
+}
