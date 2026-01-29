b14f78ffb3f687e0e8263b0458b69985cdb21c45
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-01-30 00:18:47 +0600

    30 jan 2026 recursion test sheet

---
+++ cf_Count Vowels.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int v(char q[],int n,int i){
+int o;
+o=0;
+if(q[i]=='a'||q[i]=='e'||q[i]=='i'||q[i]=='o'||q[i]=='u'){o=1;}
+if(i+1<n){return o+v(q,n,i+1);}
+else{return o;}
+}
+int main()
+{
+    int n,m,o,l,u;
+    char a[2001];
+    gets(a);
+    n=strlen(a);
+    cout<<v(a,n,0);
+
+    return 0;
+}
