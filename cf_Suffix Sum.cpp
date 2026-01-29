91e24305318a482be74e8986967fa0f7d30a3e31
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-01-29 23:57:23 +0600

    29 jan 2026

---
+++ cf_Suffix Sum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int ss(int a[],int n,int m,int i){
+    int v;
+    v=a[i];
+
+if(i+1<n){
+ if(i<n-m){ v=0; }
+ return v+ss(a,n,m,i+1);
+}
+}
+int main()
+{
+    int a[100],i,j,k,m,n;
+
+    cin>>n>>m;
+    for(i=0;i<n;i++){cin>>a[i];}
+    cout<<ss(a,n,m,0);
+    return 0;
+}
