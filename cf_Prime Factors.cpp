29be2e7c7688863c4f193b765ffcf8e70e3de9c9
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-01 14:43:35 +0600

    1 feb 2026 math and geometry

--- cf_Prime Factors.cpp
+++ cf_Prime Factors.cpp
@@ -1,3 +1,66 @@
+/*
+#include<bits/stdc++.h>
+
+using namespace std;
+int p(int n)
+{
+    int i,j,k,l,m;
+
+   m=sqrt(n);
+   l=1;
+   for(i=2;i<=m;i++){ if(n%i==0){l=0; break;} }
+   if(n==1){l=0;}
+
+
+
+    return l;
+}
+
+
+
+int ip(vector<int>&a,int n, int c){
+   if(c==0){return 0;}
+   if(c<=n){ return a[c-1]; }
+   else{  return p(c);  }
+
+}
+
+int main()
+{   ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    int n,m,i,j,k,l,o,p[1300000],c[100000],d[100000];
+    m=20000000;
+    vector<int>a(m,1);
+    k=sqrt(m);
+    //for(i=0;i<m;i++){a[i]=1;}
+    a[0]=0;
+    for(i=2;i<=k;i++){ if(a[i-1]==1){  for(j=i*i;j<=m;j+=i){ a[j-1]=0; } } }
+    l=0;
+     for(i=0;i<m;i++){if(a[i]==1){p[l]=i+1;l++; } }
+    k=0;
+    cin>>n;
+
+    if(ip(a,m,n)==1||n==1){ k++; c[0]=n;d[0]=1; }
+    else{
+       o=0;
+       j=0;
+     while(n!=1){
+
+       while(n%p[o]!=0){o++;}
+       while(n%p[o]==0){n=n/p[o];j++;}
+       c[k]=p[o];
+       d[k]=j;
+       j=0;
+       k++;
+     }
+    }
+
+    for(i=0;i<k;i++){ cout<<"("<<c[i]<<"^"<<d[i]<<")"; if(i==k-1){cout<<"\n";}else{ cout<<"*"; } }
+
+    return 0;
+}
+
+*/
+
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
