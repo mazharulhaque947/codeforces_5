3ddf21eb35f8c45d733aa169accd513a89e4ab97
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-01 06:17:55 +0600

    1 feb 2026 math and geometry

--- cf_Prime checking.cpp
+++ cf_Prime checking.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
-int main()
+int32_t main()
 {
     int n,i,j,k,l,m;
     cin>>n;
    m=sqrt(n);
    l=1;
    for(i=2;i<=m;i++){ if(n%i==0){l=0; break;} }
-
+   if(n==1){l=0;}
 
     if(l==1){cout<<"YES\n";}
     else{ cout<<"NO\n"; }
