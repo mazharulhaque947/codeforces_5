118e471e32d65d36ca734cdbace6df94ebd860ea
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-01 06:05:38 +0600

    1 feb 2026 math and geometry

---
+++ cf_Power Of Two.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j,k,l,m;
+    cin>>n;
+    l=0;
+    m=1;
+    while( (n&m)==0){m=m<<1;l++;}
+   // cout<<l<<"\n";
+    cout<<(1<<l)<<"\n";
+   // if(1<<l==n){cout<<"YES\n";}
+    else{ cout<<"NO\n"; }
+
+    return 0;
+}
