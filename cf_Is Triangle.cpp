f973c2b79df6c484033130d98b2c0bb435d805cb
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-02 01:35:12 +0600

    2 feb 2026 math and geometry

--- cf_Is Triangle.cpp
+++ cf_Is Triangle.cpp
@@ -8,7 +8,7 @@ double s(int a[])
     p=p/2;
    // if(a[0]*a[0]+a[1]*a[1]==a[2]*a[2]){  p=a[0]*a[1]*.5; }else
     { p=sqrt(  p*(p-a[0])*(p-a[1])*(p-a[2]) );}
-     cout<<p<<"\n";
+    // cout<<p<<"\n";
     return p;
 
 }
@@ -19,7 +19,7 @@ int main()
     sort(a,a+3);
 
 
-    if( a[2]<=a[0]+a[1]){cout<<"Valid\n"; cout<<s(a);}
+    if( a[2]<a[0]+a[1]){cout<<"Valid\n"; cout<<s(a);}
     else{ cout<<"Invalid\n"; }
 
     return 0;
