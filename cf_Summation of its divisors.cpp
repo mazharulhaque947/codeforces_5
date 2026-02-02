dab9e087f45d4fa5b409cce911dddc73348dc4cc
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-02 21:40:08 +0600

    2 feb 2026 math and geometry

---
+++ cf_Summation of its divisors.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int a,b,c,d,g,h,i,j;
+   // cin>>a>>b;
+   cin>>a;
+  b=0;
+  g=sqrt(a);
+
+  for(i=1;i<=g;i++){ if(a%i==0){b=b+i+a/i; } }
+  if(g*g==a){ b=b-g; }
+  cout<<b;
+    return 0;
+}
+
+
