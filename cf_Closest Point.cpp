d9a21ae8d8b9d3ffe718eaa7b290801718695e7f
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-06 06:50:56 +0600

    6 feb 2026

---
+++ cf_Closest Point.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+int i,j,k,l,m,n,t;
+cin>>t;
+while(t--)
+{
+vector<int>a;
+cin>>k;
+
+for(i=0;i<k;i++){ cin>>m; a.push_back(m);}
+
+if(k<=2){cout<<"YES\n";}
+else{cout<<"NO\n";}
+a.clear();
+}
+
+return 0;
+}
