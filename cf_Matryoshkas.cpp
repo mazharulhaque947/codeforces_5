722952ea671b0016c0a127059039c1d12ecc5b1a
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-09 02:37:51 +0600

    9 feb 2026 dynamic memory

--- cf_Matryoshkas.cpp
+++ cf_Matryoshkas.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int f(int a[],int b[],int c[],int n,int l,int k,int v)
+int f(int a[],int b[],int n,int l,int k,int v)
 {
-    int i,j,u,o,m,s,p;
-
+    int i,j,u,o,m,s,p,t;
+    vector<int>c;
     s=0;
     p=b[l]-1;
     u=0;
@@ -13,12 +13,12 @@ int f(int a[],int b[],int c[],int n,int l,int k,int v)
     {
         m=0; if(a[i]-v>0){m=1;}
 
-        if(p+1!=b[i]){ if(m==1&&u==1){u=0; c[s]=i-1; s++; } }
+        if(p+1!=b[i]){ if(m==1&&u==1){u=0; t=i-1; c.push_back(t);s++; } }
 
-        if(u==0&&m==1){c[s]=i; s++;}
-        else if(u==1&&m==0){c[s]=i-1; s++;}
+        if(u==0&&m==1){t=i; c.push_back(t); s++;}
+        else if(u==1&&m==0){t=i-1; c.push_back(t); s++;}
 
-        if(i==k&&m==1){c[s]=i; s++;}
+        if(i==k&&m==1){t=i; c.push_back(t); s++;}
         u=m;
         p=b[i];
 
@@ -31,7 +31,7 @@ int f(int a[],int b[],int c[],int n,int l,int k,int v)
 
     for(i=0;i<s;i++){
 
-         p=p+f(a,b,c,n,c[2*i],c[2*i+1],v+1);
+         p=p+f(a,b,n,c[2*i],c[2*i+1],v+1);
 
     }
 
@@ -61,7 +61,7 @@ int main()
         if(i==n-1){a[o]=m;b[o]=p;o++;}
        }
      // for(i=0;i<o;i++){cout<<a[i]<<" "<<b[i]<<"\n";}
-       k=f(a,b,d,o,0,o-1,0);
+       k=f(a,b,o,0,o-1,0);
        cout<<k<<"\n";
 
 
