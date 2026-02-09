b00d1576c5052dfb2f73d0e8798b3957cb8b9d22
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-09 23:58:31 +0600

    9 feb 2026

---
+++ cf_Lovely Palindromes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,i,k;
+    string a;
+
+    cin>>a;
+
+    cout<<a;
+
+    for(auto it = a.rbegin();it!=a.rend();it++){ cout<<*it; }
+
+    cout<<"\n";
+    return 0;
+}
