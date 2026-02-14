526328fcb7d7de8a90f4bf2d8448d62084f9ce95
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-14 10:27:34 +0600

    14 feb 2026

--- cf_We Were Both Children.cpp
+++ cf_We Were Both Children.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-int a[200],c[200],d[200],i,j,k,n,m,t,o,p;
+int a[200000],c[200000],d[200000],i,j,k,n,m,t,o,p;
 cin>>t;
 while(t--){
-m=200;
+m=200000;
 for(i=0;i<m;i++){c[i]=d[i]=0;}
  cin>>n;
 o=0;
@@ -21,9 +21,9 @@ if(c[j-1]>0){  d[j-1]=d[j-1]+c[i-1];
 if(d[j-1]>d[p]){p=j-1;} }
 }
 }
-if(o==1){o=c[0];}
-o=o+d[p];
-cout<<o<<"\n";
+
+
+cout<<p+1<<"\n";
 
 }
 return 0;
