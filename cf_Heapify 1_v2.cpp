8b706e58e87e55fb725adc9132a647c554d092d5
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-02-17 01:44:22 +0600

    17 feb 2026 try_again

--- cf_Heapify 1_v2.cpp
+++ cf_Heapify 1_v2.cpp
@@ -1,21 +1,4 @@
 
-/*
-  local code shoes no
-  codeforces says yes
-  or 48
-
-2 5 21 4 11 20 15 3 32 22 25 1 9 7 26
-
-46 6 35 34 33 40 42 29 28 8 12 45 43 23 24
-
-38 31 41 16 37 18 47 17 19 39 27 36 44 48 10
-
-30 13 14 test case
-
-codeforces 1080 div 3 b 3rd test 8901 token
-
-*/
-
 #include<bits/stdc++.h>
 # define int long long
 using namespace std;
@@ -27,11 +10,11 @@ int pp(int a,int b)
     r=1;
     while(b>0){
 
-       // if(b%2==1){ r=r*a; }
-       // b=b/2;
-       // a=a*a;
-       r=r*a;
-       b--;
+       if(b%2==1){ r=r*a; }
+       b=b/2;
+       a=a*a;
+      // r=r*a;
+      // b--;
 
     }
     return r;
@@ -52,28 +35,28 @@ int32_t main()
         //m++;
         q=n-1;
         if(n%2==0){q--;}
-        cout<<n<<" "<<m<<"\n";
+        //cout<<n<<" "<<m<<"\n";
         m++;
-        while(m<=q){ cout<<q<<" "<<a[q]<<"...\n";
-         if(a[q]!=q+1){k=0; } q-=2;  }
+        while(m<=q){ //cout<<q<<" "<<a[q]<<"...\n";
+         if(a[q]!=q+1){k=0; break;} q-=2;  }
         m=n/2;
         m++;
          u=v=0;
         for(;u<m;){
-            cout<<u<<"u ";
+            //cout<<u<<"u ";
             o=0;
             p=0;
             v=u;
             if(v+0>=m){break;}
-            while(c[v]==1){cout<<v<<"v ";
+            while(c[v]==1){//cout<<v<<"v ";
             if(v+0>=n){break;}
             v++;}
             u=v+1;
             if(v+0>=m){break;}
-            while( v+1 <=n){c[v]=1; o++;p=p+a[v]; cout<<o<<"o "<<v<<"v "<<p<<"p ";
+            while( v+1 <=n){c[v]=1; o++;p=p+a[v]; //cout<<o<<"o "<<v<<"v "<<p<<"p ";
              v=2*v+1; }
-            cout<<"\n";
-          if(o!=0){ if( p!=u*(pp(2,o)-1) ){ cout<<p<<" "<<u*(pp(2,o)-1)<<"\n"; k=0; }}
+            //acout<<"\n";
+          if(o!=0){ if( p!=u*(pp(2,o)-1) ){ k=0; break; }}
 
         }
 
