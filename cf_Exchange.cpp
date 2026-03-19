8e5e17b2e92e866b4823beccfb58940d3e7ba313
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-03-20 00:32:21 +0600

    20 march 2026

---
+++ cf_Exchange.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace srd;
+int main()
+{
+    int n,t,k,l,j,m,i,o,p;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>o>>p;
+        if(p<o){ cout<<1<<"\n"; }
+        else{  m=n/o; if(m*o!=n){m++;} cout<<m<<"\n"; }
+
+    }
+    return 0;
+}
