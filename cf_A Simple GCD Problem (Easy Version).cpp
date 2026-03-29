67af2af0256b3adf4dadecfccb6cf6e7ac45ef94
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-03-30 02:52:46 +0600

    30 march 2026

--- cf_A Simple GCD Problem (Easy Version).cpp
+++ cf_A Simple GCD Problem (Easy Version).cpp
@@ -27,10 +27,10 @@ int main()
         k=a[0];
          for(i=1;i<n;i++){ k=gg(a[i],k); }
             l=0;
-            if(k!=a[0]&&gg(a[0],a[1])==k){l++; a[0]=k;}
+            if(a[0]!=gg(a[0],a[1])){l++; a[0]=gg(a[0],a[1]);}
 
-         for(i=1;i<n-1;i++){ if(gg(a[i-1],a[i])==gg(a[i],a[i+1])&&k==gg(a[i],a[i+1])&&k!=a[i]){l++; a[i]=k;}  }
-          if((a[n-1]!=a[n-2])){l++;}
+         for(i=1;i<n-1;i++){ if( gg(a[i],a[i-1])*gg(a[i],a[i+1])<a[i]){l++; a[i]= gg(a[i],a[i-1])*gg(a[i],a[i+1]);}  }
+          if(a[n-1]!=gg(a[n-1],a[n-2])){l++;}
        cout<<l<<"\n";
 
     }
