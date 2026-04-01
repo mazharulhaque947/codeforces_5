756d11d866cadee62cb48e3fc117e01b99b0bb39
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-01 22:28:42 +0600

    1 april 2026

--- cf_Bad Ugly Numbers.cpp
+++ cf_Bad Ugly Numbers.cpp
@@ -11,7 +11,7 @@ int main()
     {
         cin>>n;
         if(n==1){cout<<"-1\n";}
-        else{ l=n%2;  for(i=0;i<n;i++){ cout<<a[ (i+l)%2 ] }  }
+        else{ l=n%2;  for(i=0;i<n;i++){ cout<<a[ (i+l)%2 ]; } cout<<"\n"; }
 
     }
     return 0;
