06cab4bebbd7e409b678c25eed3bbd19bf187529
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-01 22:09:04 +0600

    1 april 2026

---
+++ cf_Bad Ugly Numbers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i,j,l,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        if(n==1){cout<<"-1\n";}
+        else{ for(i=0;i<n-1;i++){ cout<<"5"; } cout<<"3\n"; }
+
+    }
+    return 0;
+}
