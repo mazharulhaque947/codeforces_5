420b4c198732cbc0b7bae4d97e6ea6b88d15ff17
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-01 17:31:28 +0600

    1 april 2026

---
+++ cf_Circle Game.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a[100],i,j,k,l,t,n,m;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        for(i=0;i<n;i++){
+
+            cin>>a[i];
+        }
+
+        if(n%2==1){cout<<"M\n";}
+        else{
+            l=1;
+            for(i=0;i<n;i+=2){ if(a[i]<=a[i+1]){ l=0; break; } }
+            if(l==1){ cout<<"Mike\n"; }
+            else{  cout<<"Joe\n"; }
+        }
+
+    }
+
+    return 0;
+}
