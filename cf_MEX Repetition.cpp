9ce873322d3e485b3ba0af9bb6ede9675d3431f6
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-03 05:38:05 +0600

    3 april 2026

--- cf_MEX Repetition.cpp
+++ cf_MEX Repetition.cpp
@@ -17,7 +17,7 @@ int main()
         b[ n ]=o;
         for(i=0;i<n;i++){  b[ i ]= a[i];  }
             for(i=0;i<1+n;i++){  cout<<b[i]<<" "; } cout<<"\n";
-         for(i=0;i<n;i++){ cout<<b[ (n-(k%(n)+1 )+i)%(n+1) ]<<" "; } cout<<"\n";
+         for(i=0;i<n;i++){ cout<<b[ (n-((k-1)%(n) )+i)%(n+1) ]<<" "; } cout<<"\n";
     }
 
     return 0;
