ea440d11b2c6e41b0e7183eab37561587b0a2dbd
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-03 05:24:51 +0600

    3 april 2026

--- cf_MEX Repetition.cpp
+++ cf_MEX Repetition.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int a[100],b[100],i,j,k,l,m,n,o,p,t;
+    int a[100],b[101],i,j,k,l,m,n,o,p,t;
     cin>>t;
     while(t--)
     {
@@ -14,10 +14,10 @@ int main()
         p=0;
         for(i=0;i<n;i++){ cin>>a[i]; p=p+a[i]; }
         o=o-p;
-        b[ ((k-1)+0)%n ]=o;
-        for(i=1;i<n;i++){  b[ ((k-1)+i)%n ]= a[i-1]; }
+        b[ n ]=o;
+        for(i=0;i<n;i++){  b[ i ]= a[i]; }
 
-         for(i=0;i<n;i++){ cout<<b[i]<<" "; } cout<<"\n";
+         for(i=0;i<n;i++){ cout<<b[ (n-(k%n )+i)%(n+1) ]<<" "; } cout<<"\n";
     }
 
     return 0;
