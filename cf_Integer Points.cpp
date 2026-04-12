0c1e063a5b81063b784000f8c89d6a3563c8459c
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-12 22:25:44 +0600

    12 april 2026

--- cf_Integer Points.cpp
+++ cf_Integer Points.cpp
@@ -10,14 +10,18 @@ int main()
     {
 
         cin>>n;
-
-        for(i=0;i<n;i++){ cin>>a[i]; }
+         l=0;
+        for(i=0;i<n;i++){ cin>>a[i]; l=l+a[i]%2; }
         sort(a,a+n);
         k=0;
 
         cin>>m;
 
-        for(i=0;i<m;i++){  cin>>b[i];  for(j=0;j<n;j++){  if((a[j]+b[i])%2==0&& (b[i]-a[j])%2==0 ){k++;} } }
+        for(i=0;i<m;i++){  cin>>b[i];  if(b[i]%2==1){ k=k+l; }else{ k=k+(n-l); }
+
+        //for(j=0;j<n;j++){  if((a[j]+b[i])%2==0&& (b[i]-a[j])%2==0 ){k++;} }
+
+         }
         cout<<k<<"\n";
     }
 
