878c64602039332644acbbfe7a903fb01c02895d
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-14 07:39:43 +0600

    14 april 2026

--- cf_Little Elephant and Magic Square.cpp
+++ cf_Little Elephant and Magic Square.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int a[3],b[3][3],c[3],i,j,k,l,o,p,m,n,v;
+    int a[3],b[3][3],c[3],i,j,k,l,o,p,m,n,v,nn;
 
     for(i=0;i<3;i++){ cin>>b[i][0]>>b[i][1]>>b[i][2]; }
     o=b[0][1]+b[0][2];
@@ -22,7 +22,8 @@ int main()
     sort(a,a+3);
     k=0+a[2];
     o=0;
-    while(-c[0]+k+o+b[0][1]+b[0][1]!=-c[0]+k-c[1]+k-c[2]+k+3*o){o++;}
+    nn=0;
+    while(-c[0]+k+o+b[0][1]+b[0][2]!=-c[0]+k-c[1]+k-c[2]+k+3*o){cout<<o<<"\n";o++; }
      for(i=0;i<3;i++){
         for(j=0;j<3;j++){ if(i==j){ cout<<-c[i]+k+o<<" "; } else{ cout<<b[i][j]<<" "; }} cout<<"\n";
 
