10f7996cb8a79344534810888ea53fe150e195da
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-16 23:59:08 +0600

    16 april 2026

---
+++ cf_Berland Poker.cpp
@@ -0,0 +1,25 @@
+
+//Berland Poker
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int a,b,c,i,j,k,n,m,t,o,p;
+cin>>t;
+while(t--)
+{
+cin>>n>>m>>k;
+o=n/k;
+if(o<=m){ a=o; b=m-o; }
+else{ a=m; b=0;   }
+c=b%(k-1);
+if(c==0){ c=b/(k-1);  }
+else{  c=b/(k-1) +b%(k-1); }
+p=a-c;
+if(p<0){  p=0;}
+cout<<p<<"\n";
+}
+return 0;
+}
