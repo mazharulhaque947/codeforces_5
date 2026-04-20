8c348bcdb591d5cc4c55d87d483aaf7afcd3489a
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-20 22:32:27 +0600

    20 april 2026

---
+++ cf_Factorise N+M.cpp
@@ -0,0 +1,18 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    int main()
+    {
+        int n,t;
+        cin>>t;
+        while(t--)
+        {
+
+            cin>>n;
+            if(n==2){ cout<<n<<"\n"; }
+            else{ cout<<3<<"\n";  }
+
+        }
+
+        return 0;
+    }
