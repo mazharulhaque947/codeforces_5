d94399c2b7d5ba6b4d8b49cd5598a548d4c2bcc4
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-21 23:05:59 +0600

    21 april 2026

---
+++ cf_A Number Between Two Others.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t,a,b,c,n,m,k,l,o,p,i,j,q;
+
+    cin>>t;
+    while(t--){
+      cin>>a>>b;
+      c=b/2;
+
+      o=c/a;
+      o++;
+      p=b/a;
+    p--;
+      if(p-o>=0){ cout<<"YES\n"; }
+      else{ cout<<"NO\n";}
+
+
+    }
+
+    return 0;
+}
