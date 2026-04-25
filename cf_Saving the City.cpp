7b8ec01be303b6bc2379f1b5976569d836d10845
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-25 22:58:18 +0600

    25 april 2026

--- cf_Saving the City.cpp
+++ cf_Saving the City.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int a[100],i,j,k,m,n,o,p,l,t,b,c;
+    int a[100],i,j,k,m,n,o,p,l,t,b,c,mm,oo,ll;
     string q;
 
     cin>>t;
@@ -21,14 +21,24 @@ int main()
         m=l=p=0;
 
         for(i=0;i<n;i++){ if(q[i]==q[p]){m++;}else{ a[l]=m; m=1; p=i;l++; } if(i==n-1){ a[l]=m; l++;  } }
+        ll=l;
         if(l%2==1){ if(o==2){l--;} }
         else{ if(o==1){l--;}   }
-       m=1;
+        // m=1;  if(l==1&&o==2){ m--; }
+        m=0;
         for(i=o;i<l;i+=2){ m++; }
-        p=0;
-        for(i=o;i<l;i+=2){ if(a[i]*c<((b))){ m--; p++;} }
+        mm=0;
+        oo=1;
+        if(q[0]=='1'){oo--;}
 
-        cout<<m*b+p*c<<"\n";
+        if(oo==0){ if(ll%2==1){  } else{ ll--; } }
+        else{ if(ll%2==1){ ll--; } else{ } }
+         for(i=oo;i<ll;i+=2){ mm++; }
+       // if(m==0){m++;}
+        p=0;
+        for(i=o;i<l;i+=2){ if(a[i]*c<((b))){ mm--; p+=a[i]*c;} }
+
+        cout<<mm*b+p<<"\n";
 
     }
 
