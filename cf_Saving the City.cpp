06103262e371d13d206b7466cd749596eec72f8c
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-25 22:05:48 +0600

    25 april 2026

--- cf_Saving the City.cpp
+++ cf_Saving the City.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -24,7 +23,7 @@ int main()
         for(i=0;i<n;i++){ if(q[i]==q[p]){m++;}else{ a[l]=m; m=1; p=i;l++; } if(i==n-1){ a[l]=m; l++;  } }
         if(l%2==1){ if(o==2){l--;} }
         else{ if(o==1){l--;}   }
-       m=0;
+       m=1;
         for(i=o;i<l;i+=2){ m++; }
         p=0;
         for(i=o;i<l;i+=2){ if(a[i]*c<((b))){ m--; p++;} }
