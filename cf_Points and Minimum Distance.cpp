9bf3434dc4c736a915324d07b71cb8214e6cdf14
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-26 23:35:39 +0600

    26 april 2026

---
+++ cf_Points and Minimum Distance.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[100],i,n,j,k,l,x[100],y[100],t;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        for(i=0;i<2*n;i++){cin>>a[i];}
+        sort(a,a+n);
+        for(i=0;i<n;i++){x[i]=a[i];y[i]=a[n+i];}
+        k=0;
+        for(i=0;i<n-1;i++){ l=x[i+1]-x[i]; if(l<0){l=l*-1;} k=k+l; l=y[i+1]-y[i]; if(l<0){l=l*-1;} k=k+l;}
+
+        cout<<k<<"\n";
+         for(i=0;i<n;i++){cout<<x[i]<<" "<<y[i]<<"\n";}
+
+    }
+
+    return 0;
+}
