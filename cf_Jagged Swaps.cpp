000cf9a36d0ca1b726163aae3779148cd4eff2e7
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-26 23:57:07 +0600

    26 april 2026

---
+++ cf_Jagged Swaps.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h1.
+
+using namespace std;
+
+int main()
+{
+    int a[10],i,j,k,t,n;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        for(i=0;i<n;i++){ cin>>a[i]; }
+        if(a[0]==1){ cout<<"YES\n"; }
+        else{ cout<<"NO\n"; }
+
+    }
+
+    return 0;
+}
