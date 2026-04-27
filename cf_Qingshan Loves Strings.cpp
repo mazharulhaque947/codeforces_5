de0dcc57b2d9ca89c0e0c2f00085b38a536b8a75
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-28 01:13:12 +0600

    28 april 2026

--- cf_Qingshan Loves Strings.cpp
+++ cf_Qingshan Loves Strings.cpp
@@ -31,7 +31,7 @@ int main()
          else{
 
               if(v==0&&y==1){ if(x!=k&&ol(w,m)==1&&m%2==1){ cout<<"YES\n";  }else{ cout<<"NO\n"; }  }
-              else{ if(x!=k&&ol(w,m)==1&&m%2==1){ cout<<"YES\n";  }else{ cout<<"NO\n"; }  }
+              else if(x!=k&&ol(w,m)==1&&m%2==1){ cout<<"YES\n";  }else{ cout<<"NO\n"; }  }
               else{ cout<<"NO\n"; }
 
 
