118750ffbd5df6258cedf6cd2d489b4119e30d30
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-28 23:08:02 +0600

    28 april 2026

---
+++ cf_A Number Between Two Others_v2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int a[100],l,k,i,j,m,n,o,p,t;
+
+//FILE *F1;
+//F1=freopen("lab1b.txt","r",stdin);
+cin>>t;
+//Plain text
+for(j=0;j<t;j++)
+{
+cin>>n;
+cin>>m;
+
+   o=m/n;
+   p=(o-1)*n;
+
+     if(p<=n){//cout<<"YES\n";
+     a[j]=-1;}
+     else{//cout<<"NO\n";
+     a[j]=p;}
+
+}
+
+//fclose(F1);
+
+//FILE *F2;
+//F2=freopen("lab1b_ans.txt","w",stdout);
+for(j=0;j<t;j++){
+
+    cout<<a[j]<<"\n";
+}
+
+//fclose(F2);
+return 0;
+//Plain text
+}
