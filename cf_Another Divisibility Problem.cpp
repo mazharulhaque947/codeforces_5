020c48101fd32f65edf1c37828ef21fea600b14e
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-04-28 23:41:05 +0600

    28 april 2026

---
+++ cf_Another Divisibility Problem.cpp
@@ -0,0 +1,21 @@
+// discussed by professor in algorithm lab
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,t;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        cout<<2*n<<"\n";
+
+
+    }
+
+
+    return 0;
+}
