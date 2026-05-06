d18702f0b1648e65174dc4c6988529aa07b33db6
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-06 23:03:22 +0600

    6 may 2026

---
+++ cf_Zhily and Array Operating.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[100],i,j,k,l,m,n,t,p;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        p=0;
+        for(i=0;i<n;i++){cin>>a[i];}
+        for(i=0;i<n-1;i++){if(a[i]<=0&&a[i+1]+a[i]>0){ a[i]=a[i]+a[i+1]; } }
+        for(i=0;i<n;i++){ if(a[i]>0){p++;} }
+        cout<<p<<"\n";
+    }
+
+    return 0;
+}
