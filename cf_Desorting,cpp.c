0869fec69b4cb8dc27b4db48e5fe9a8d2c2f5977
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-08 23:57:09 +0600

    8 may 2026

--- cf_Desorting,cpp.c
+++ cf_Desorting,cpp.c
@@ -12,7 +12,7 @@ int main()
 
         k=1000000000;
 
-        for(i=1;i<n;i++){ o=a[i]-a[i-1]; if(o<0){k=0; break;} o=o/2 +o%2 ; if(o<k){k=o;} }
+        for(i=1;i<n;i++){ o=a[i]-a[i-1]; if(o<0){k=0; break;} if(o==0){k=1; } o=o/2 +1 ; if(o<k){k=o;} }
         printf("%d\n",k);
     }
 
