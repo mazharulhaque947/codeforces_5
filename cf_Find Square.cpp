97ef2990ba55e2bba4cb0085ae6042370281deee
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-09 23:58:05 +0600

    9 may 2026

---
+++ cf_Find Square.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a[115],b[10000],c[10000],i,j,k,l,m,n,t;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>m;
+        l=0;
+
+        for(i=0;i<n;i++){ cin>>a[i]; }
+
+         for(i=0;i<n;i++){
+
+            for(j=0;j<m;j++){ if(a[i][j]=='B'){ b[l]=i; c[l]=j; l++; } }
+         }
+
+         cout<<(b[l-1]-b[0]+1)/2<<" "<<(c[l-1]-c[0]+1)/2<<"\n";
+    }
+
+    return 0;
+}
