c34d073c7d36ad2fe37bc4e105fa1cc48afedbd8
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-11 22:12:57 +0600

    11 may 2026

--- cf_Discounts.cpp
+++ cf_Discounts.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main()
 {
     int a[100],b[100],i,j,k,l,m,n,s,p,t;
-    cin>>t;
-    while(t--)
-    {
+
         cin>>n;
         s=0;
         for(i=0;i<n;i++){cin>>a[i];s+=a[i];}
@@ -13,6 +11,6 @@ int main()
         cin>>m;
         for(i=0;i<m;i++){cin>>b[i];cout<<s-a[n-b[i]]<<"\n";}
 
-    }
+
     return 0;
 }
