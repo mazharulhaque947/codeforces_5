b6e6991087b9b3d6c14c938b646fc4ca834ac5ae
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-16 23:47:07 +0600

    16 may 2026

---
+++ cf_Find Array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[100],i,j,k,l,n,m,t;
+
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+
+        for(i=0;i<n;i++){ cout<<i+2<<" "; }
+         cout<<"\n";
+
+
+    }
+
+    return 0;
+}
