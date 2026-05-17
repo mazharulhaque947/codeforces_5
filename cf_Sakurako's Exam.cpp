f63be43ec4686b784728210ae3310a509c0277ae
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-17 18:01:22 +0600

    17 may 2026

---
+++ cf_Sakurako's Exam.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int s,i,j,k,l,m,n,t;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>m>>n;
+
+        if(n%2==0&&m%2==0){ cout<<"YES\n"; }
+        else if(m%2==1&&n>=2&&n%2==0){ cout<<"YES\n";  }
+
+        else{ cout<<"NO\n";  }
+
+    }
+
+
+    return 0;
+}
