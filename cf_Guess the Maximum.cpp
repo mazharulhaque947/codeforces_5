7ab2350b772cbfbdf220a3bc13373d02d8c6f613
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-17 18:48:59 +0600

    17 may 2026

--- cf_Guess the Maximum.cpp
+++ cf_Guess the Maximum.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    int a[4000],i,j,k,l,m,n,o,t,p;
+    long long a[4000],i,j,k,l,m,n,o,t,p;
 
-    o=1e9;
+
     cin>>t;
 
     while(t--)
-    {
+    {   o=1000000000;
         cin>>n;
-
-        for(i=0;i<n;i++){ cin>>a[i]; if(i!=0){ p=max(a[i],a[i-1])-1; if(p<o){o=p;} } }
+        p=1000000000;
+        for(i=0;i<n;i++){ cin>>a[i]; if(i!=0){ p=a[i]; if(p<a[i-1]){p=a[i-1]; } p--; cout<<p<<"\n"; if(p<o){o=p;} } }
         cout<<o<<"\n";
 
     }
