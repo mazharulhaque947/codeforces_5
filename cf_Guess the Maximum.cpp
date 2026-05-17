5b63802a4ddb4c7b5487a0b16eda081479f0cb31
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-17 18:34:12 +0600

    17 may 2026

---
+++ cf_Guess the Maximum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[4000],i,j,k,l,m,n,o,t,p;
+
+    o=1e9;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+
+        for(i=0;i<n;i++){ if(i!=0){ p=max(a[i],a[i-1])-1; if(p<o){o=p;} } }
+        cout<<o<<"\n";
+
+    }
+
+    return 0;
+}
