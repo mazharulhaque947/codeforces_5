905504be4a37a2e33a69b064854caa3a799eac36
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-19 01:57:57 +0600

    19 may 2026

---
+++ cf_Equal Candies.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a[100],i,j,k,l,t,s,t;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        k=0;
+        s=0;
+        for(i=0;i<n;i++){ cin>>a[i]; s=s+a[i]; if(a[i]<a[k]){k=i;} }
+
+        cout<<s-a[k]*n<<"\n";
+
+
+    }
+
+    return 0;
+}
