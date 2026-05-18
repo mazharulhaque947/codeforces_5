edb77cfa549a1ba7596e43f3438aeedc4ac3ed72
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-19 02:11:18 +0600

    19 may 2026

---
+++ cf_Gregor and Cryptography.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int p,y;
+    cin>>y;
+
+    while(y--)
+    {
+        cin>>p;
+
+        cout<<2<<" "<<p-1<<"\n";
+    }
+
+    return 0;
+}
