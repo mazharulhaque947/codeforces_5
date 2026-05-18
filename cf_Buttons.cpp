6998fa7e21cbf54d49adfb1b8c5700b8685edcc3
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-19 01:38:47 +0600

    19 may 2026

---
+++ cf_Buttons.cpp
@@ -0,0 +1,39 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i,j,k,t,o,p,a;
+
+   cin>>t;
+
+   while(t--)
+   {
+       cin>>n>>k>>a;
+      if(n>k+a+1){ cout<<"First\n";  }
+      else if(n+a<=k){  cout<<"Second\n"; }
+      else if(n==k){
+        if(a%2==1){ cout<<"First\n";  }
+      else {  cout<<"Second\n"; }
+
+      }
+      else if(n<k){
+        a=a-(k-n);
+           if(a%2==0){ cout<<"First\n";  }
+      else {  cout<<"Second\n"; }
+
+      }
+      else{
+           a=a-(n-k);
+           if(a%2==1){ cout<<"First\n";  }
+      else {  cout<<"Second\n"; }
+
+
+      }
+
+   }
+    return 0;
+}
+
+
