3470f3460f4a44e4b9d3b38daef95ede48ce2a7f
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-19 04:27:46 +0600

    19 may 2026

---
+++ cf_Choosing Cubes.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a[100],j,i,k,l,m,n,o,p,t;
+    cin>>t;
+
+    while(t--){
+
+        cin>>n>>m>>k;
+        for(i=0;i<n;i++){cin>>a[i];}
+        m=a[m-1];
+        sort(a,a+n, greater<int>());
+
+        if(k==n){ cout<<"YES\n"; }
+        else{
+
+           if(a[k]==m&&a[k-1]==m){ cout<<"MABYE\n"; }
+           else if(a[k]<m){  cout<<"YES\n"; }
+           else{  cout<<"NO\n"; }
+
+        }
+    }
+
+    return 0;
+}
