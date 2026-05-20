cbb1ca72771a86f6b2097d7b907c76b1d459cc2f
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-21 00:03:39 +0600

    20 may 2026

--- cf_Two Elevators.cpp
+++ cf_Two Elevators.cpp
@@ -12,8 +12,8 @@ int main()
 
         cin>>a>>b>>c;
 
-        if(b<c){  b=2*(c-b)+b-1; }
-
+        if(b<c){  b=2*(c-b)+b; }
+         b--;
         if(a-1==b){cout<<"1\n";}
         else if(a-1<b){ cout<<"2\n"; }
         else{ cout<<"3\n"; }
