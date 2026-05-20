faff96bce31e7b4356001b7c62f6447f559161d8
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-20 23:58:24 +0600

    20 may 2026

---
+++ cf_Two Elevators.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,v,m,n,c,t,d,e,f;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>a>>b>>c;
+
+        if(b<c){  m=2*(c-b)+b; }
+
+        if(a==b){cout<<"1\n";}
+        else if(a<b){ cout<<"2\n"; }
+        else{ cout<<"3\n"; }
+    }
+
+    return 0;
+}
