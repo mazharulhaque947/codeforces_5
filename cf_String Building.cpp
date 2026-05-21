29d643e0c9a4074e51ec17a4b68bf2c1d6cdeda7
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-21 19:56:33 +0600

    21 may 2026

---
+++ cf_String Building.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[1000],i,j,k,l,m,n,t;
+    string q;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>q;
+        n=q.size();
+        k=l=m=0;
+
+        for(i=0;i<n;i++){
+            if(q[i]!=q[k]){ m++; }
+            else{ a[l]=m; m=1; l++; k=i; }
+            if(i==n-1){  a[l]=m; m=1; l++; k=i; }
+
+        }
+        m=1;
+        for(i=0;i<l;i++){ if(a[i]<=1){m=0; break;} }
+
+        if(m==1){cout<<"YES\n"; }
+        else{ cout<<"NO\n"; }
+
+    }
+
+    return 0;
+}
