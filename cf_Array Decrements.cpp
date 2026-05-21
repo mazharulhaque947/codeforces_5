527c293d47f9eaa6340adcea0a4720e101d68130
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-21 19:22:44 +0600

    21 may 2026

---
+++ cf_Array Decrements.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a[500],b[500],i,j,k,n,m,t,o,p;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        for(i=0;i<n;i++){ cin>>a[i]; }
+        cin>>b[0];
+
+        k=a[0]-b[1];
+        o=1;
+         for(i=1;i<n;i++){cin>>b[i]; if(a[i]-b[i]!=k){o=0;} }
+
+        if(o==1&&k>=0){cout<<"YES\n";}
+        else{cout<<"NO\n";}
+
+    }
+
+    return 0;
+}
