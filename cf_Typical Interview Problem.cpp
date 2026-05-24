c36847898e1fac573b1d29df4ca27ea04acc19a8
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-24 23:02:38 +0600

    24 may 2026

---
+++ cf_Typical Interview Problem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,k,l,m,n,o,p,t;
+    int a[8]={5,1,5,5,1,5,5,1};
+    string q;
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+        cin>>q;
+
+        m=0;
+
+        for(i=0;i<n;i++){ l=1; for(j=0;j<n;j++){ if(a[(j+i%n)]!=q[j]){ l=0; break; } }   if(m==1){break;} }
+
+        if(m==1){ cout<<"YES\n"; }
+        else{ cout<<"NO\n"; }
+    }
+
+    return 0;
+}
