5f2d62bddb62455aaef0ace25596067bef93c1b4
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-05-25 23:28:45 +0600

    25 may 2026

--- cf_Remove Prefix.cpp
+++ cf_Remove Prefix.cpp
@@ -13,10 +13,14 @@ int main()
         m=200;
         for(i=0;i<m;i++){ b[i]=0; }
         k=1;
-        for(i=0;i<n;i++){ cin>>a[i];  b[a[i]-1]++; if(b[a[i]-1]>=2){ k=0; break; } }
+        for(i=0;i<n;i++){ cin>>a[i]; }
+        o=0;
+        for(i=n;i>=1;i--){ b[a[i-1]-1]++; if(b[a[i-1]-1]>=2){ k=0; o=n-i; break; } }
 
-        if(k==0){ cout<<"NO\n"; }
-        else{ cout<<"YES\n"; }
+       // if(k==0){ cout<<"NO\n"; }
+        //else{ cout<<"YES\n"; }
+
+        cout<<o<<"\n";
     }
 
     return 0;
