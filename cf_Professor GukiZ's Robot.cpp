2a9067a242f7787a0f332e51a6950c99deab28d4
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-02 23:12:56 +0600

    2 june 2026

---
+++ cf_Professor GukiZ's Robot.cpp
@@ -0,0 +1,22 @@
+
+//Professor GukiZ's Robot
+
+int main()
+{
+int a,b,c,d,t,u,i,j,k;
+
+cin>>t;
+while(t--)
+{
+cin>>a>>b;
+cin>>c>>d;
+a=max(a,c)-min(a,c);
+b=max(b,d)-min(b,d);
+
+cout<<max(a,b)<<"\n";
+
+}
+
+return 0;
+
+}
