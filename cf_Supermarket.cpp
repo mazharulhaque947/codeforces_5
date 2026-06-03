2808e85cf9abf51d677b30e3aba4292e2c7e43cd
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-03 23:59:52 +0600

    3 june 2026

--- cf_Supermarket.cpp
+++ cf_Supermarket.cpp
@@ -1,58 +1,20 @@
-#include<bits/stdc++.h>
-#define int long long
+#include <bits/stdc++.h>
 using namespace std;
-int gg(int a,int b)
-{
 
-    if(b%a==0){ return a; }
-    else{  return gg(b%a,a);  }
+int main() {
+    int n,m;
+    double a,b;
+    cin>>n>>m;
 
-}
-int lc(int a, int b)
-{
+    double mn = 1e18;
 
-    int c;
-    c=a*b;
-    c=c/gg(a,b);
-    return c;
+    for (int i=0;i<n;i++) {
 
-}
-int32_t main()
-{
-    int n,m,i,j,k,l,o,p,t,a[500],b[500];
-
-   // cin>>t;
-
-   // while(t--)
-    {
-        cin>>n>>m;
-        l=1;
-        for(i=0;i<n;i++){
-
-            cin>>a[i]>>b[i];
-            l=lc(l,b[i]);
-
-        }
-
-        k=0;
-
-        for(i=0;i<n;i++){
-
-            if( a[i]*(l/b[i]) < a[k]*(l/b[k])  ){ k=i; }
-
-        }
-
-        double x,y;
-        x=a[k];
-        y=b[k];
-        x=x*m;
-        x=x/y;
-
-        //cout<<x<<"\n";
-        printf("%.6lf\n",x);
-
-
+        cin>>a>>b;
+        mn = min(mn, a/b);
     }
+
+printf("%.6lf\n",mn);
 
     return 0;
 }
