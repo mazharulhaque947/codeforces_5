c4bdfa809c9360adb4a0dc273db548b6846f2303
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-03 23:18:24 +0600

    3 june 2026

---
+++ cf_Supermarket.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gg(int a,int b)
+{
+
+    if(b%a==0){ return a; }
+    else{  return gg(b,b%a);  }
+
+}
+int lc(int a, int b)
+{
+
+    int c;
+    c=a*b;
+    c=c/gg(a,b);
+    return c;
+
+}
+int main()
+{
+    int n,m,i,j,k,l,o,p,t,a[500],b[500];
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>m;
+        l=1;
+        for(i=0;i<n;i++){
+
+            cin>>a[i]>>b[i];
+            l=lc(l,b[i]);
+
+        }
+
+        k=0;
+
+        for(i=0;i<n;i++){
+
+            if(a[i]*(l/b[i])<a[k]*(l/b[k])){ k=i; }
+
+        }
+
+        double x,y;
+        x=a[k];
+        y=b[k];
+        x=x*m;
+        x=x/y;
+
+        cout<<x<<"\n";
+
+
+
+    }
+
+    return 0;
+}
