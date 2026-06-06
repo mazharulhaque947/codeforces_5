11c9c6bccbbbecb42d97b3e441b095bbe1c5c738
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-06 20:09:55 +0600

    6 june 2026

---
+++ cf_Two Screens.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,k,j,l,m,n,o,p,t;
+    string q,w;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>q;
+        cin>>w;
+        n=q.size();
+        m=w.size();
+
+        o=0;
+        for(i=0;i<min(n,m);i++){ if(q[i]==w[i]){o++;} }
+
+         if(o==0){}
+         else{o=o-1; }
+
+         cout<<n+m-o<<"\n";
+
+    }
+
+    return 0;
+}
