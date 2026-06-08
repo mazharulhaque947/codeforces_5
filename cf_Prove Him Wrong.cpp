a7bdaec07c65391337d1d7ee952c584e11dcc1f0
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-09 03:04:42 +0600

    9 june 2026

---
+++ cf_Prove Him Wrong.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,m,k,n,t;
+
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+
+        if(n<=18){  cout<<"YES\n"; k=1; for(i=0;i<n;i++){ cout<<k<<" "; k*=3; } }
+
+        else{  cout<<"NO"; }
+
+        cout<<"\n";
+    }
+
+    return 0;
+}
