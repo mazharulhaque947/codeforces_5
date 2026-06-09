cbc26d85d00a5bffa82c7b51bac9315a09b63350
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-09 07:18:27 +0600

    9 june 2026

---
+++ cf_Make All Equal.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[100],c[100],i,t,j,k,m,n;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+        for(i=0;i<n;i++){ cin>>a[i]; c[i]=0; }
+        k=0;
+        for(i=0;i<n;i++){  c[a[i]-1]++; if(c[a[i]-1]>k){ k=c[a[i]-1]; }  }
+
+        cout<<k<<"\n";
+    }
+
+    return 0;
+}
