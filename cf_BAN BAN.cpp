b8c5f9318ef01b7b4f162610d84b6575c0a8e69f
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-22 21:22:51 +0600

    22 june 2026

---
+++ cf_BAN BAN.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i,j,k,l,m,i,j,t;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+
+        m=n/2;
+        m+=n%2;
+        cout<<m<<"\n";
+        for(i=0;i<m;i++){ cout<<(1+i*3)<<" "<<(n-1)*+2<<"\n"; }
+
+
+
+    }
+
+    return 0;
+}
