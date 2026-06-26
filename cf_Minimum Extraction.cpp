12b52888f07ccdc7c3ddafb5606aee12ad202e3d
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-26 22:02:59 +0600

    26 june 2026

--- cf_Minimum Extraction.cpp
+++ cf_Minimum Extraction.cpp
@@ -3,20 +3,31 @@ using namespace std;
 
 int main()
 {
-    int a[100],i,j,h,k,l,m,n,t,v;
+    int a[100],c[200],i,j,h,k,l,m,n,t,v;
     cin>>t;
     while(t--)
     {
 
         cin>>n;
 
-        for(i=0;i<n;i++){cin>>a[i];}
+        for(i=0;i<n;i++){cin>>a[i];c[i]=a[i];}
          sort(a,a+n);
         v=a[0];
         m=0;
 
-        for(i=1;i<n;i++){ m=m+a[i-1]; if(a[i]-m>v){v=a[i]-m;} }
-        cout<<v<<"\n";
+        for(i=1;i<n;i++){
+
+                c[i]=a[i]-a[i-1]; a[i]-=a[i-1];
+       // if(a[i]-a[i-1]>v){v=a[i]-a[i-1]; a[i]-=a[i-1];}else{break;}
+
+
+                // m=m+a[i-1]; if(a[i]-m>v){v=a[i]-m;}
+
+
+         }
+         sort(c,c+n);
+
+        cout<<c[n-1]<<"\n";
     }
 
 
