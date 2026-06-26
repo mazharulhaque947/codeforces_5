81bf3c9571b7101108ca0edf671512bdbabf7964
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-26 22:37:16 +0600

    26 june 2026

--- cf_Minimum Extraction.cpp
+++ cf_Minimum Extraction.cpp
@@ -16,8 +16,13 @@ int main()
         m=0;
 
         for(i=1;i<n;i++){
+                m=m+a[i-1];
+                if(a[i]-m>v){v=a[i]-m;}
+                a[i]-=a[i-1];
 
-               if(a[i]>v){v=a[i];}  c[i]=a[i]-a[i-1]; a[i]-=a[i-1]; if(a[i]>v){v=a[i];}
+
+              // if(a[i]>v){v=a[i];}
+               // c[i]=a[i]-a[i-1]; a[i]-=a[i-1]; if(a[i]>v){v=a[i];}
        // if(a[i]-a[i-1]>v){v=a[i]-a[i-1]; a[i]-=a[i-1];}else{break;}
 
 
