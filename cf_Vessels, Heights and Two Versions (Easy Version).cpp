7da922d6d0e08494013a9a939f973a6a9006b2c7
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-06-28 17:21:47 +0600

    28 june 2026 tutorial

--- cf_Vessels, Heights and Two Versions (Easy Version).cpp
+++ cf_Vessels, Heights and Two Versions (Easy Version).cpp
@@ -1,3 +1,5 @@
+// help from codeforces tutorial
+
 #include<bits/stdc++.h>
 using namespace std;
 int mix(int a,int b){ int c =a; if (b<a){c=b;} return c; }
@@ -17,12 +19,12 @@ int main()
             for(j=0;j<n;j++){l[j]=r[j]=0;}
             for(j=1;j<n;j++){r[(i+j)%n]=max(a[(i+j-1)%n],r[(i+j-1)%n]);}
             for(j=1;j<n;j++){l[(i-j+n)%n]=max(a[(i+n-j)%n],l[(i-j+n+1)%n]);}
-            for(j=0;j<n;j++){cout<<r[j]<<" ";} cout<<"\n";
-            for(j=0;j<n;j++){cout<<l[j]<<" ";} cout<<"\n";
+           // for(j=0;j<n;j++){cout<<r[j]<<" ";} cout<<"\n";
+           // for(j=0;j<n;j++){cout<<l[j]<<" ";} cout<<"\n";
 
             s=0;
             for(j=0;j<n;j++){ s+=mix(l[j],r[j]); }
-            cout<<s<<"= ";
+            cout<<s<<" ";
         }
          cout<<"\n";
 
