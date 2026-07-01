639336ce8dd231cef630e43792069276a97c0f9e
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-07-02 01:53:54 +0600

    2 july 2026

---
+++ cf_Simple Palindrome.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string q="aeiou";
+    int i,j,k,l,m,n,o,p,t;
+    cin>>t;
+    while(t--)
+    {
+
+        cin>>n;
+        m=n/5;
+        p=n%5;
+
+        if(m>0){
+            for(i=0;i<n;i++)
+            {
+
+                o=0;
+                if(i<p){o=1;}
+                for(j=0;j<o+m;j++){cout<<q[i];}
+
+            }
+        }
+        else{
+
+            for(i=0;i<n;i++){cout<<q[i];}
+        }
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
