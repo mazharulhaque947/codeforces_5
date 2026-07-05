c3070f6f16a2af9c87e16bae87d0e7c2dea2aa80
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-07-05 23:02:23 +0600

    5 july 2026

---
+++ cf_Barrels.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int a[2000],i,j,k,l,m,n,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>k;
+        for(i=0;i<n;i++){cin>>a[i];}
+        sort(a,a+n,greater<int>());
+        l=0;
+        for(i=0;i<k+1;i++){l+=a[i];}
+        cout<<l<<"\n";
+
+    }
+
+    return 0;
+}
