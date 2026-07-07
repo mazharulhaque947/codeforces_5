9b96ea8bcd193c1b934a22790213006dd180fa62
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-07-08 00:50:48 +0600

    8 july 2026

---
+++ cf_Coprime.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[1000],b[200],c[200],i,j,k,l,m,n,o,p,t;
+    cin>>t;
+    while(t--)
+    {  m=100;
+        for(i=0;i<m;i++){a[i]=-1;}
+       cin>>n;
+       l=0;
+       for(i=0;i<n;i++){cin>>b[i];if(a[b[i]-1]==-1){c[l]=b[i]-1;l++;}a[b[i]-1]=i;}
+       k=-1;
+
+       for(i=0;i<l;i++){cout<<c[i]<<" ";} cout<<"\n";
+
+        for(i=0;i<l;i++){
+                for(j=i;j<l;j++){  m=c[i]+1; o=c[j]+1;
+                    if(m>o){swap(m,o);}
+                    if( o%m!=0){ if(k<a[m-1]+a[o-1]){k=a[m-1]+a[o-1];} }
+                }
+        }
+       cout<<k<<"\n";
+
+    }
+    return 0;
+}
