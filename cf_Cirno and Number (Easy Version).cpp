61e5ebfc075a2c9c18bd6acbd1d3507eb1e81bd1
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-07-16 13:52:57 +0600

    16 july 2026

--- cf_Cirno and Number (Easy Version).cpp
+++ cf_Cirno and Number (Easy Version).cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long pp(int a)
+long long a[300][20];
+long long pp(long long a)
 {
     long long c,d;
     d=2;
@@ -17,32 +18,34 @@ long long pp(int a)
     return c;
 }
 
-long long nn(long long a[100][20],long long  c[2],long long  i,long long l)
+long long nn(long long a[300][20],long long  c[2],long long  i,long long l)
 {
-    int j,k,m,o;
+    long long j,k,m,o;
     o=0;
 
     for(j=0;j<l;j++){o=o*10+c[ a[i][j] ];  }
     return o;
 }
 int main()
-{
-    long long a[100][20],i,j,k,l,m,n,o,p,t,b,c[2];
-
-    cin>>t;
-    while(t--)
-    {
-        cin>>b>>n;
-        m=100;
-        k=15;
+{   ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    long long i,j,k,l,m,n,o,p,t,b,c[2];
+      m=300;
+        k=19;
 
         for(i=0;i<m;i++){ for(j=0;j<k;j++){ a[i][j]=0; } }
         for(i=0;i<m;i++){ o=i; l=0; while(o>0){ a[i][l]=o%2; o/=2; l++; } }
        // for(i=0;i<m;i++){ for(j=0;j<k;j++){ cout<<a[i][j]<<""; } cout<<"\n"; }
 
+    cin>>t;
+    while(t--)
+    {
+
+
+        cin>>b>>n;
+
         for(i=0;i<n;i++){cin>>c[i];}
         //  cout<<nn(a,c,4,3)<<"\n";
-        if(a==0){ cout<<min(c[0],c[1])<<"\n"; }
+        if(b==0){ cout<<min(c[0],c[1])<<"\n"; }
         else{
 
            l=log10(b)+1;
