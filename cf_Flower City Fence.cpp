1e9cbf3deaf4d9de046668e9222990e6a07b28e2
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-07-24 23:34:35 +0600

    24 july 2026

--- cf_Flower City Fence.cpp
+++ cf_Flower City Fence.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int a[200],b[200],i,o,k,l,j,m,n,t;
+    int a[200],b[200],c[200],d[200],f[200],i,p,o,k,l,j,m,n,t;
 
     cin>>t;
 
@@ -13,20 +13,35 @@ int main()
         cin>>n;
 
         for(i=0;i<n;i++){ cin>>a[i]; }
+        l=0;
+        o=0;
+        m=0;
 
+        for(i=0;i<n;i++){
+
+                if(a[i]==a[o]){ m++; }
+                else{ c[l]=a[o]; f[l]=m; m=1; o=i; l++; }
+
+                if( i==n-1 ){  c[l]=a[o]; f[l]=m; m=1; o=i; l++;  }
+        }
+          //   for(i=0;i<l;i++){ cout<<c[i]<<" "<<f[i]<<"\n";  }
+        p=l;
         l=1;
         m=0;
         j=n-1;
         for(  ; true ; ){
            if(j<0){break;}
-           if( a[j]>=l ){ b[m]=j+1; m++; l++; }
+           if( a[j]>=l ){ b[m]=j+1;d[m]=a[j]-l+1; m++; l=a[j]+1;; }
            else{j--;}
 
 
         }
+       // for(i=0;i<m;i++){ cout<<b[i]<<" "<<d[i]<<"\n";  }
         k=1;
-        for(i=0;i<min(n,m);i++){ if(a[i]!=b[i]){k=0; break;} }
-
+        if(p!=m){k=0;}
+        else{
+        for(i=0;i<m;i++){ if(c[i]!=b[i] || f[i]!=d[i] ){k=0; break;} }
+        }
         if(k){cout<<"YES";}
         else{cout<<"NO";}
          cout<<"\n";
