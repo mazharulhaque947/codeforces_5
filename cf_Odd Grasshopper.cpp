06ab7b63e42a6819b25392d7269d13757d59d14e
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-07-27 22:10:34 +0600

    27 july 2026

--- cf_Odd Grasshopper.cpp
+++ cf_Odd Grasshopper.cpp
@@ -3,16 +3,17 @@
 using namespace  std;
 int main()
 {
-    long long i,j,k,l,t;
+    long long i,j,k,l,t,v;
     cin>>t;
 
     while(t--)
     {
 
         cin>>k>>l;
-
-        if(k%2==1){  j=l/4;  if(l%4==0){ k+=0;  }else if(l%4==1){ k+=j*4+1; }else if(l%4==2){ k--; }else{ k=k-j*8-4; } }
-        else{   j=l/4;  if(l%4==0){ k+=0;  }else if(l%4==1){ k-=j*4+1; }else if(l%4==2){ k++; }else{ k=k+j*8+4; } }
+       v=k;
+       if(v<0){v*=-1;}
+        if(k%2==1){  j=l/4;  if(l%4==0){ k+=0;  }else if(l%4==1){ k+=l; }else if(l%4==2){ k--; }else{ k=k-l-1; } }
+        else{   j=l/4;  if(l%4==0){ k+=0;  }else if(l%4==1){ k-=l; }else if(l%4==2){ k++; }else{ k=k+l+1; } }
         cout<<k<<"\n";
     }
 
