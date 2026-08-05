8b18b9c3f19d838425c423fc5e13d6c948eca98f
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-08-05 22:10:41 +0600

    5 aug 2026

---
+++ cf_Little Fairy's Painting.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[1000],i,j,k,l,m,n,o,p,t,b[100];
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+        for(i=0;i<n;i++){ cin>>a[i]; }
+
+        sort(a,a+n);
+         l=0;
+         p=0;
+        for(i=0;i<n;i++){
+
+            if(a[i]==a[p]){ }
+            else{ b[l]=a[p]; p=i; l++; }
+            if(i==n-1){  b[l]=a[p]; l++; }
+        }
+
+        //cout<<*upper_bound(b,b+l,l)<<"\n";
+        o=*upper_bound(b,b+l,l);
+         if(o==0){ o=b[l-1]; }
+         cout<<o<<"\n";
+    }
+
+    return 0;
+}
