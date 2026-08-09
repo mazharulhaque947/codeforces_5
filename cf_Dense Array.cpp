8ac95880e26032c7eba6fe3be8271d7514f70f42
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-08-10 03:53:04 +0600

    10 aug 2026

---
+++ cf_Dense Array.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[50],i,j,k,l,m,n,t;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+        l=0;
+        for(i=0;i<n;i++){ cin>>a[i];
+
+           if(i>0){
+
+               k=max(a[i],a[i-1])/min(a[i],a[i-1]);
+
+               m=1;j=0;
+               while(m*2<k){
+                m*=2;j++;
+               }
+               l+=j;
+
+             //  if(k>=2 //&&k*min(a[i],a[i-1])!=max(a[i],a[i-1])
+
+                 // ){ l+=k-2; }
+           }
+
+         }
+
+ cout<<l<<"\n";
+
+
+    }
+
+
+    return 0;
+}
