7e864db86452b04304c4b3a6bd0f85bcece73da2
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-08-11 04:43:17 +0600

    11 aug 2026

--- cf_Monsters.cpp
+++ cf_Monsters.cpp
@@ -11,11 +11,11 @@ int main()
     {
         mm.clear();
         cin>>n>>k;
-        for(i=0;i<n;i++){ cin>>a[i];o=a[i]/k; if(o*k!=a[i]){o++;}  b[i]=k; mm.push_back({o,n-i});}
+        for(i=0;i<n;i++){ cin>>a[i];o=a[i]/k; if(o*k!=a[i]){o++;}  b[i]=k; mm.push_back({o,i});}
         sort(mm.begin(),mm.end());
         for(auto x :mm){
 
-            cout<<n-x.second+1<<" ";
+            cout<<x.second+1<<" ";
         }
        cout<<"\n";
     }
