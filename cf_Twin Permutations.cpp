a8c8efe493343962156c5ef65a25c9faff18535d
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-08-21 19:35:21 +0600

    22 aug 2026

---
+++ cf_Twin Permutations.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[100],n,m,i,j,t;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+
+        for(i=0;i<n;i++){ cin>>a[i];  cout<<n+1-a[i]<<" "; }
+          for(i=0;i<n;i++){   cout<<n+1-a[i]<<" "; }
+        cout<<"\n";
+
+    }
+
+    return 0;
+}
