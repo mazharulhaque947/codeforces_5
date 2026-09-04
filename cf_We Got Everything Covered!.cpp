77017479b28c8bfa7b88b1fc8792c562be624151
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-09-05 05:31:33 +0600

    5 sep 2026

---
+++ cf_We Got Everything Covered!.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,k,l,m,n,t;
+
+    cin>>t;
+
+
+    while(t--){
+
+        cin>>n>>m;
+
+        for(i=0;i<n;i++){
+
+            for(j=0;j<m;j++){ cout<< (char)('a'+j) ;}
+
+        }
+
+       cout<<"\n";
+
+    }
+
+
+    return 0;
+}
